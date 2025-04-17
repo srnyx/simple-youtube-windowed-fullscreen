6f67986fc93f1343d53ac302c6fe453544f0a46c
srnyx <25808801+srnyx@users.noreply.github.com>
2025-04-16 21:37:11 -0400

    1.1.0: Resolve #2
    Default keybind is Ctrl+Shift+W

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.action.onClicked.addListener(tab => {
+function createWindow(tab) {
     if (!tab.url || !tab.url.includes('youtube.com/watch?')) return;
     void chrome.windows.create({
         url: `https://youtube.com/embed/` + new URL(tab.url).searchParams.get('v'),
@@ -7,4 +7,12 @@ chrome.action.onClicked.addListener(tab => {
         height: 540
     });
     void chrome.tabs.remove(tab.id);
+}
+
+// When extension is clicked on in toolbar
+chrome.action.onClicked.addListener(createWindow);
+
+// When keyboard shortcut is pressed
+chrome.commands.onCommand.addListener((command, tab) => {
+    if (command === 'windowed-fullscreen') createWindow(tab);
 });
