d08e548f6fca1a4944ed8dc81dcbc303f93f7486
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-27 23:43:58 -0400

    Move extension files into Extension folder

--- Package.sh
+++ Package.sh
@@ -6,18 +6,15 @@ GREEN='\033[0;32m'
 L_GREEN='\033[1;92m'
 YELLOW='\033[0;33m'
 
-# The files/folders to be included in the package
-FILES=(
-  "icons"
-  "manifest.json"
-  "background.js")
-
 # Extract version from manifest.json
-VERSION=$(grep -oP '"version": "\K[0-9.]+(?=")' manifest.json)
+VERSION=$(grep -oP '"version": "\K[0-9.]+(?=")' "Extension/manifest.json")
 
 # Create ZIP file using 7-Zip
 ZIP_FILE="Simple YouTube Windowed Fullscreen $VERSION.zip"
-7z a -tzip "$ZIP_FILE" "${FILES[@]}" > /dev/null
+(
+  cd "Extension" || exit
+  7z a -tzip "../$ZIP_FILE" ./* ./.* > /dev/null
+)
 
 # Notify user
 echo -e "${GREEN}Files packaged into ${L_GREEN}$ZIP_FILE${GREEN} successfully\n${YELLOW}"
